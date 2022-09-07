51a6dc82f4faf76d042a16109f9e1c777aa185bc
Mariana Pinto <mariana.vendasbb@gmail.com>
2022-09-06 22:45:59 -0300

    user registration finished

--- 01.Cadastro de Usuario/script.js
+++ 01.Cadastro de Usuario/script.js
@@ -3,7 +3,10 @@ let lastName = prompt('Informe o sobrenome nome do recruta:');
 let fieldOfStudy = prompt('Qual o campo de estudo do recruta?');
 let yearOfBirth = prompt('Qual o ano de nascimento do reccruta?');
 
-let year = ('2022');
-
-let age = parseFloat('year - birth')
+alert(
+    'Recruta cadastrado com sucesso!\n' +
+    '\nNome completo: ' + firstName + ' ' + lastName + '.' +
+    '\nCampo de estudo: ' + fieldOfStudy + '.' +
+    '\nIdade: ' + (2022 - yearOfBirth) + '.'
+)
 
