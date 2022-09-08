ea32ebb43262eacf31322deba56bf0850c783f37
Mariana Pinto <mariana.vendasbb@gmail.com>
2022-09-08 11:50:15 -0300

    exercise finished

---
+++ 11.Pilha de Cartas/pilha.js
@@ -0,0 +1,32 @@
+const baralho = []
+let option = ""
+
+// Menu interativo
+do {
+    option = prompt(
+        "Cartas no baralho: " + baralho.length + 
+        "\n1. Adicionar uma carta\n2. Puxar uma carta\n3. Sair."
+    )
+
+    switch (option) {
+        case "1":
+            const novaCarta = prompt("Qual é a carta?")
+            baralho.push(novaCarta)
+            break
+        case "2":
+            const cartaPuxada = baralho.pop()
+            if (!cartaPuxada) {
+             alert("Não há nenhuma carta no baralho!")
+            } else {
+             alert("Você puxou um(a) " + cartaPuxada)
+            }
+            break
+        case "3":
+            alert("Saindo...")
+            break
+           default:
+            alert("Opção Inválida!")
+
+    }
+} while (option !== "3")
+
